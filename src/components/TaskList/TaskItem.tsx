2b06118d6e85d80242e02ccd1bc9c2a540e25cab
Abenezer <abitysank@gmail.com>
2025-12-11 18:29:21 -0500

    update to tasklist structure setting up

--- src/components/TaskList/TaskItem.tsx
+++ src/components/TaskList/TaskItem.tsx
@@ -1,6 +1,39 @@
 import React from "react";
-import type { TaskItemProps, TaskStatus} from "../types";
+import type { TaskItemProps, TaskStatus } from "../types";
 
-export const TaskItem: React.FC<TaskItemProps> = ({}) => {
-    return <></>
-}
+export const TaskItem: React.FC<TaskItemProps> = ({
+    task,
+    onStatusChange,
+    onDelete,
+}) => {
+    return (
+        <div className="card mb-3">
+            <div>
+                <h4>{task.title}</h4>
+                <span></span>
+            </div>
+            <div>
+                <p>{task.description}</p>
+                <div>
+                    <div>Due Date: </div> {""}
+                    <span>{task.dueDate}</span>
+                </div>
+                <div>
+                    <strong>Priority:</strong>
+                    <span></span>
+                </div>
+            </div>
+
+            <div>
+                <select className="" value={} onChange={}>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                </select>
+                <button className="" onClick={}>
+                    Delete
+                </button>
+            </div>
+        </div>
+    );
+};

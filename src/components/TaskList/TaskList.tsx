e0e2335216e4de40b1b2b4b32c39f3227cf0adf5
Abenezer <abitysank@gmail.com>
2025-12-11 20:26:06 -0500

    task item done

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -2,6 +2,31 @@ import React from "react";
 import { TaskItem } from "./TaskItem";
 import type { TaskListProps } from "../types";
 
-export const TaskList: React.FC<TaskListProps> = ({}) => {
-    return <></>;
+export const TaskList: React.FC<TaskListProps> = ({
+    tasks,
+    onStatusChange,
+    onDelete,
+}) => {
+    if (tasks.length === 0) {
+        return (
+            <div>
+                <div>
+                    <h5>No tasks found</h5>
+                    <p>Create new Task or try adjusting your filters</p>
+                </div>
+            </div>
+        );
+    }
+    return (
+        <div>
+            {tasks.map((task) => (
+                <TaskItem
+                    key={task.id}
+                    task={task}
+                    onStatusChange={onStatusChange}
+                    onDelete={onDelete}
+                />
+            ))}
+        </div>
+    );
 };
